b6ea3e8845488ba1de6dc310b2a0f3f2e017fbfb
Satoshi Takano <satoxi.tacano@gmail.com>
2013-07-17 16:17:58 +0900

    bugfixes for advanced.tween.Bounce

--- src/advanced/tween.js
+++ src/advanced/tween.js
@@ -165,7 +165,7 @@ new Namespace("advanced.tween").use(function() {
         * @param d {Number} number of total steps
         * @memberOf Bounce
         */
-        def(function easeOut(t, b, c, d) {
+        $$.def(function easeOut(t, b, c, d) {
                 if ((t /= d) < (1 / 2.75)) { return c * (7.5625 * t * t) + b; }
                 else if (t < (2 / 2.75)) { return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b; }
                 else if (t < (2.5 / 2.75)) { return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b; }
@@ -180,7 +180,7 @@ new Namespace("advanced.tween").use(function() {
         * @param d {Number} number of total steps
         * @memberOf Bounce
         */
-        def(function easeInOut(args) {
+        $$.def(function easeInOut(args) {
             var s = 1.70158;
             if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
             return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
